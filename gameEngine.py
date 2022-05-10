65cc15c47780daf6fd464c8c71b0d85a77d6bde8
wilson-freitas <wjmf@cin.ufpe.br>
2022-05-10 20:20:50 -0300

    incrased background size and created counters for collectables

--- gameEngine.py
+++ gameEngine.py
@@ -1,8 +1,12 @@
 from variables import *
 
 
-def game_engine(flag_soma, background, pitu, carro_azul, carro_verde, texto, fonte, fonte_padrao, relogio, tela, x_verde, y_verde, x_azul, y_azul, x_pitu, y_pitu, x_bitcoin, y_bitcoin, bitcoin):
+def game_engine(velocidade, contador_pitu, pitu_points, pitu_points_str, points_str, points, flag_soma, background, pitu, carro_azul, carro_verde, texto, fonte, fonte_padrao, relogio, tela, x_verde, y_verde, x_azul, y_azul, x_pitu, y_pitu, x_bitcoin, y_bitcoin, bitcoin):
     while True:
+        points_str = str(points)
+        pitu_points_str = str(pitu_points)
+        texto = fonte.render(points_str, 1,(255,255,255))
+        contador_pitu = fonte.render(pitu_points_str, 1,(255,255,255))
         relogio.tick(60)
         tela.fill((0, 0, 0))
         tela.blit(background, (0, 0))
@@ -11,45 +15,48 @@ def game_engine(flag_soma, background, pitu, carro_azul, carro_verde, texto, fon
                 pygame.quit()
                 exit()
             if event.type == KEYDOWN:
-                if event.key == K_a and x_verde >= 140:
+                if event.key == K_a and x_verde >= 285:
                     x_verde -= 10
-                elif event.key == K_d and x_verde <= 629:
+                elif event.key == K_d and x_verde <= 916:
                     x_verde += 10
                 elif event.key == K_w and y_verde >= 10:
                     y_verde -= 10
-                elif event.key == K_s and y_verde <= 450:
+                elif event.key == K_s and y_verde <= 685:
                     y_verde += 10
 
     #Define a movimentação do carro verde, respeitando os limites da estrada da imagem de fundo
         if pygame.key.get_pressed()[K_a]:
-            if(x_verde >= 140): x_verde -= 9
+            if(x_verde >= 285): x_verde -= velocidade
         elif pygame.key.get_pressed()[K_d]:
-            if(x_verde <= 629): x_verde += 9
+            if(x_verde <= 916): x_verde += velocidade
         elif pygame.key.get_pressed()[K_w]:
-            if(y_verde >= 10): y_verde -= 9
+            if(y_verde >= 10): y_verde -= velocidade
         elif pygame.key.get_pressed()[K_s]:
-            if(y_verde <= 450): y_verde += 9
+            if(y_verde <= 685): y_verde += velocidade
 
     #calls game_Screen
         tela.blit(carro_verde,(x_verde,y_verde))
         tela.blit(carro_azul,(x_azul,y_azul))
         tela.blit(pitu,(x_pitu,y_pitu))
-        tela.blit(texto,(440,10))
+        tela.blit(contador_pitu,(75,180))
+        tela.blit(pitu, (15, 155))
+        tela.blit(bitcoin,(10,55))
+        tela.blit(texto,(75,65))
         tela.blit(bitcoin,(x_bitcoin, y_bitcoin))
         y_azul += flag_soma
-        y_pitu += 9
-        y_bitcoin += 9
+        y_pitu += flag_soma
+        y_bitcoin += flag_soma
     #Condicionao para que, se os objetos interativos passarem do limite inferior, volta para cima
 
         if y_azul > altura:
             y_azul = -100
-            x_azul = randint(160,629)
+            x_azul = randint(285,916)
         if y_pitu > altura:
             y_pitu = -80
-            x_pitu = randint(160,629)
+            x_pitu = randint(300,916)
         if y_bitcoin > altura:
             y_bitcoin = -70
-            x_bitcoin = randint(160, 629)
+            x_bitcoin = randint(305,916)
 
         if y_verde < (y_azul + 120):
             if y_verde < y_azul - 150:
@@ -64,8 +71,10 @@ def game_engine(flag_soma, background, pitu, carro_azul, carro_verde, texto, fon
             else:
                 if (x_verde > x_pitu and x_verde < (x_pitu + 65)) or ((x_verde + 75) > x_pitu and x_verde < x_pitu):
                     y_pitu = -80
-                    x_pitu = randint(160,629)
-                    flag_soma += 0.7
+                    x_pitu = randint(300,916)
+                    flag_soma += 0.3
+                    velocidade += 0.05
+                    pitu_points += 1
         
         if y_verde < (y_bitcoin + 40):
             if y_verde < y_bitcoin - 150:
@@ -73,10 +82,12 @@ def game_engine(flag_soma, background, pitu, carro_azul, carro_verde, texto, fon
             else:
                 if (x_verde > x_bitcoin and x_verde < (x_bitcoin + 45)) or ((x_verde + 60) > x_bitcoin and x_verde < x_bitcoin):
                     y_bitcoin = -70
-                    x_bitcoin = randint(160,629)
+                    x_bitcoin = randint(305,916)
                     
                     barulho_moeda = pygame.mixer.music.load('coin_sound.wav')
                     pygame.mixer.music.play()
                     pygame.mixer.music.set_volume(0.1)
                     
+                    points += 1
+
         pygame.display.update()             
